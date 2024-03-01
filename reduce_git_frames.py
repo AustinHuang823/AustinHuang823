e0bce339787e547cea29d8b4582f7f924df7d4ad
Austin Huang <b03502035@gmail.com>
2024-02-29 18:21:33 -0800

    gif optimization

--- reduce_git_frames.py
+++ reduce_git_frames.py
@@ -1,21 +1,25 @@
 from PIL import Image, ImageSequence
 
-def reduce_gif_frames(input_gif_path, output_gif_path, reduction_factor):
+def reduce_and_extend_gif_duration(input_gif_path, output_gif_path, reduction_factor, total_duration_seconds):
     with Image.open(input_gif_path) as img:
         frames = [frame.copy() for frame in ImageSequence.Iterator(img)]
         
         reduced_frames = frames[::reduction_factor]
         
+        total_duration_ms = total_duration_seconds * 1000
+        new_frame_duration = total_duration_ms // len(reduced_frames)
+        
         reduced_frames[0].save(
             output_gif_path,
             save_all=True,
             append_images=reduced_frames[1:],
-            loop=0
+            loop=0,
+            duration=new_frame_duration
         )
 
-# Example usage
-input_gif_path = 'assets/gif/segmentation_origin.gif'
-output_gif_path = 'assets/gif/segmentation.gif'
-reduction_factor = 10 # Adjust this to keep every nth frame
+input_gif_path = 'assets/gif/rdkdc_imagedrawing_origin.gif'
+output_gif_path = 'assets/gif/rdkdc_imagedrawing.gif'
+reduction_factor = 10
+total_duration_seconds = 5
 
-reduce_gif_frames(input_gif_path, output_gif_path, reduction_factor)
+reduce_and_extend_gif_duration(input_gif_path, output_gif_path, reduction_factor, total_duration_seconds)
